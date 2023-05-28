6be4b6b8caf74077b26d421383007a96f41d806f
ROHIT VISHWANATH GUPTA <88444883+rohitg1010@users.noreply.github.com>
2023-05-28 19:39:47 +0530

    Time: 4 ms (38.84%) | Memory: 6.1 MB (39.39%) - LeetSync

--- the-kth-factor-of-n/the-kth-factor-of-n.cpp
+++ the-kth-factor-of-n/the-kth-factor-of-n.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int kthFactor(int n, int k) {
-        vector<int> fact;
-        for(int i=1;i<=n;i++) if(n%i==0) fact.push_back(i);
-        // fact.push_back(n);
-        if(fact.size()<k) return -1;
-        // for(auto it:fact) cout<<it<<" ";
-        // cout<<endl;
-        return fact[k-1];
+        int cnt=0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0) cnt++;
+            if(cnt==k) return i;
+        }
+        return -1;
     }
 };
