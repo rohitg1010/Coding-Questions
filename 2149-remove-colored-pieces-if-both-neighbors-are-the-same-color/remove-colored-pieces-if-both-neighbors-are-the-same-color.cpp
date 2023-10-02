cf49b889281a47bccfd18e8d93a59e1b963d0674
ROHIT VISHWANATH GUPTA <88444883+rohitg1010@users.noreply.github.com>
2023-10-02 23:57:44 +0530

    Time: 130 ms (5.24%) | Memory: 13.7 MB (17.42%) - LeetSync

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -5,13 +5,14 @@ public:
         if(colors.size()<=2) return false;
         for(int i=0;i<colors.size()-2;i++){
             string s=colors.substr(i,3);
-            cout<<s<<endl;
+            // cout<<s<<endl;
             if("AAA"==s) count_a++;
             if("BBB"==s) count_b++;
         }
-        cout<<count_a<<" "<<count_b<<endl;
-        if(count_a==count_b) return false;
-        if(count_a>count_b) return true;
-        return false;
+        // cout<<count_a<<" "<<count_b<<endl;
+        // if(count_a==count_b) return false;
+        // if(count_a>count_b) return true;
+        // return false;
+        return count_a>count_b;
     }
 };
