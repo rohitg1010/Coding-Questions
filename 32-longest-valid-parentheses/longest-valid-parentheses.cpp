367f1c812062f32bb834bfd154cbd53ba0941bc1
ROHIT VISHWANATH GUPTA <88444883+rohitg1010@users.noreply.github.com>
2023-10-10 00:19:17 +0530

    Time: 244 ms (5.22%) | Memory: 7.3 MB (58.60%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -5,7 +5,9 @@ public:
         for(int i=0;i<s.size();i++){
             if(s[i]=='('){
                 int count=0;
+                // cout<<i<<endl;
                 for(int j=i;j<s.size();j++){
+                    // cout<<j<<" ";
                     if(s[j]=='(') count++;
                     else count--;
                     if(count==0) ans=max(ans,j-i+1);
@@ -14,6 +16,7 @@ public:
                         break;
                     }
                 }
+                cout<<endl;
             }
         }
         return ans;
