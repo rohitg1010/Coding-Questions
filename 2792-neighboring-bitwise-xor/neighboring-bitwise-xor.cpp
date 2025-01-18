301a9b7b3f6e4303211e0aad642b6ee7bcd9fa02
ROHIT VISHWANATH GUPTA <88444883+rohitg1010@users.noreply.github.com>
2025-01-18 21:38:12 +0530

    Time: 15 ms (16.46%) | Memory: 275.5 MB (23.42%) - LeetSync

---
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool doesValidArrayExist(vector<int>& derived) {
+        int n=derived.size();
+        vector<int> org(n);
+
+        //Populating the org array
+        for(int i=1; i<n; i++){
+            org[i]=derived[i-1]^org[i-1];
+        }
+
+        //Validating the rules for index i
+        for(int i=0; i<n; i++){
+            int idx=(i+1)%n;
+            if(derived[i]!=(org[i]^org[idx])) return false;
+        }
+
+        return true;
+    }
+};
