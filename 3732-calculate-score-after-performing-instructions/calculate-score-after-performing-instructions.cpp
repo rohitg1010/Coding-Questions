8f10164ca214a1bd1b2a25659abbb9a08efaa77c
ROHIT VISHWANATH GUPTA <88444883+rohitg1010@users.noreply.github.com>
2025-05-20 21:05:45 +0530

    Time: 4 ms (62.58%) | Memory: 166.8 MB (98.03%) - LeetSync

---
+++ 3732-calculate-score-after-performing-instructions/calculate-score-after-performing-instructions.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long calculateScore(vector<string>& instructions, vector<int>& values) {
+        long long score = 0;
+        int i=0,n=values.size();
+        while(i>=0&&i<n){
+            if(instructions[i]=="-1") return score;
+            if(instructions[i]=="add"){
+                score+=values[i];
+                instructions[i++]="-1";
+            }
+            else{
+                instructions[i]="-1";
+                i=i+values[i];
+            }
+        }
+        return score;
+    }
+};
