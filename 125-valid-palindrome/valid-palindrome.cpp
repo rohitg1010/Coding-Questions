eaa44585eed0542779c4620f258fd4ce349de4dc
ROHIT VISHWANATH GUPTA <88444883+rohitg1010@users.noreply.github.com>
2025-07-19 18:02:50 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (99.43%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
+    char isValid(char c){
+        if((c>='a'&&c<='z')||(c>='0'&&c<='9')) return c;
+        if(c>='A'&&c<='Z') return c+32;
+        return '*';
+    }
     bool isPalindrome(string s) {
-        string og="";
-        for(auto it:s){
-            if(it>='a'&&it<='z') og+=it;
-            else if(it>='A'&&it<='Z') og+=(it+32);
-            else if(it>='0'&&it<='9') og+=it;
-        }
-        int i=0,j=og.size()-1;
+        int i=0,j=s.size()-1;
         while(i<j){
-            if(og[i]!=og[j]) return false;
-            else{
-                i++;j--;
+            char c1 = isValid(s[i]);
+            char c2 = isValid(s[j]);
+            if(c1=='*'){
+                i++;
+                continue;
             }
+            if(c2=='*'){
+                j--;
+                continue;
+            }
+            if(c1!=c2) return false;
+            i++; j--;
         }
         return true;
     }
