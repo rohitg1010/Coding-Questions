dcfdb70f860579e76fd139114a6086daad15260b
ROHIT VISHWANATH GUPTA <88444883+rohitg1010@users.noreply.github.com>
2025-12-10 14:12:41 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (50.54%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,24 +1,55 @@
 class Solution {
 public:
+
+void row (vector<vector<int>>& matrix , int i , int j , int n , int m)
+{
+    for (int j = 0 ; j < m ; j++ )
+    {
+        if (matrix[i][j] != 0)
+    matrix[i][j] = -1000000000 ;
+    }
+}
+
+void col(vector<vector<int>>& matrix , int i , int j , int n , int m)
+{
+    for (int i = 0 ; i < n ; i++)
+    {
+        if (matrix [i][j] != 0)
+    matrix[i][j] = - 1000000000 ;
+    }
+}
+
     void setZeroes(vector<vector<int>>& matrix) {
-        int n=matrix.size(),m=matrix[0].size();
-        bool row=false,col=false;
-        for(int i=0;i<m;i++) if(matrix[0][i]==0){ row=true; break;}
-        for(int i=0;i<n;i++) if(matrix[i][0]==0){ col=true; break;}
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                if(matrix[i][j]==0){
-                    matrix[i][0]=0;
-                    matrix[0][j]=0;
-                }
-            }
+        
+int n = matrix.size() ; 
+int m = matrix[0].size() ; 
+vector <int> answer ; 
+
+for (int i = 0 ; i < n ; i++)
+{
+
+    for (int j = 0 ; j < m ; j++)
+    {
+        if (matrix[i][j] == 0)
+        {
+            row(matrix , i , j , n , m) ; 
+            col(matrix , i , j , n , m) ;
         }
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                if(matrix[i][0]==0||matrix[0][j]==0) matrix[i][j]=0;
-            }
-        }
-        if(row)for(int i=0;i<m;i++) matrix[0][i]=0;
-        if(col)for(int i=0;i<n;i++) matrix[i][0]=0;
+    }
+}
+
+
+for (int i = 0 ; i < n ; i++)
+{
+    for (int j = 0 ; j < m ; j++)
+    {
+        if (matrix[i][j] == -1000000000)
+        matrix[i][j] = 0 ;
+    }
+}
+
+return ;
+
+
     }
 };
