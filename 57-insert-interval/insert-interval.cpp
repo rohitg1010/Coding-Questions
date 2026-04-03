ed6ffd9d6745aa884ee278c2e9681e642a48c564
ROHIT VISHWANATH GUPTA <88444883+rohitg1010@users.noreply.github.com>
2026-04-03 23:37:02 +0530

    Time: 7 ms (7.23%) | Memory: 22.4 MB (7.85%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,20 +1,72 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        intervals.push_back(newInterval);
-        sort(intervals.begin(),intervals.end());
-        vector<vector<int>> ans;
-        ans.push_back(intervals[0]);
-        int k = 0,n=intervals.size();
-        for(int i=1;i<n;i++){
-            if(intervals[i][0]<=ans[k][1]){
-                ans[k][1]=max(intervals[i][1],ans[k][1]);
-            }
-            else{
-                ans.push_back(intervals[i]);
-                k++;
-            }
-        }
-        return ans;
+    vector<vector<int>> freq ; 
+    bool ans = true ;
+
+    if (intervals.size() == 0)
+    {
+         freq.push_back(newInterval) ;
+ return freq ;
+    }
+ 
+
+for(auto &it : intervals)
+{
+    if (newInterval[0] <= it[1] && newInterval[1] >= it[0])
+    {
+        it [1] = max(it[1]  , newInterval[1]) ; 
+        it [0] = min (it[0] , newInterval[0]) ; 
+    
+       // cout <<it[0] <<endl;
+        //cout<<it[1] << endl;
+        ans = false ;
+        break ;
+        
+    }
+}
+
+
+
+// for (auto it : intervals)
+// {
+//    cout <<it[0] <<endl;
+//         cout<<it[1] << endl;
+// }
+if (ans == true)
+{
+     intervals.push_back(newInterval) ;
+     sort(intervals.begin() , intervals.end()) ;
+}
+vector<int> temp = intervals[0] ; 
+
+
+
+// for (auto it : intervals)
+// {
+//     cout << it[0] <<endl ; 
+//     cout <<it[1] << endl;
+// }
+
+for (auto it : intervals)
+{
+    if (it[0] <= temp[1] && it[1]>= temp[0])
+    {
+        temp[1] = max(temp[1] , it[1]) ;
+    }
+    else{
+        freq.push_back(temp) ; 
+        cout << temp[0] << endl;
+           cout << temp[1] << endl;
+        temp = it ; 
+    }
+}
+
+freq.push_back(temp) ;
+
+
+
+return freq;
+
     }
 };
