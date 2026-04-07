48e5e408cfa16aea5bd15456649d97d48b630050
ROHIT VISHWANATH GUPTA <88444883+rohitg1010@users.noreply.github.com>
2026-04-07 23:49:45 +0530

    Time: 3 ms (67.03%) | Memory: 14.9 MB (36.35%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,12 +1,32 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n=nums.size();
+        //Brute Force
+        /*int n=nums.size();
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 if(nums[i]+nums[j]==target) return {i,j};
             }
         }
         return {-1,-1};
+        */
+        //Two Pointer - Sorting keeping index
+        vector<pair<int, int>> numsWithIndex;
+        for (int i = 0; i < nums.size(); i++) {
+            numsWithIndex.push_back({nums[i], i});
+        }
+        sort(numsWithIndex.begin(), numsWithIndex.end());
+        int left = 0, right = nums.size() - 1;
+        while (left < right) {
+            int sum = numsWithIndex[left].first + numsWithIndex[right].first;
+            if (sum == target) {
+                return {numsWithIndex[left].second, numsWithIndex[right].second};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return {};
     }
 };
