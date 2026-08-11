abdf33cec5a93fe651a157ba410c3dc3a1be6101
ROHIT VISHWANATH GUPTA <88444883+rohitg1010@users.noreply.github.com>
2026-08-11 17:30:53 +0530

    Time: 466 ms (15.69%) | Memory: 225.5 MB (13.05%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,14 +1,34 @@
 class Solution {
 public:
+
+
+int answer(vector<int>& prices , int index , int buy , int count , vector<vector<vector<int>>>&dp)
+{
+
+if (count == 0 || index >= prices.size())
+return 0 ;
+
+if (dp[index][buy][count] != -1)
+return dp[index][buy][count] ; 
+
+
+if (buy == 1)
+{
+    return   dp[index][buy][count] = max(-prices[index] + answer(prices , index + 1 , 0 , count ,dp) , 0 + answer(prices , index +1 , 1 , count ,dp)) ;
+}
+
+
+return  dp[index][buy][count] =  max(prices[index] + answer(prices , index + 1 , 1 , count-1 ,dp) , 0 + answer(prices , index +1 , 0 , count ,dp)) ;
+
+
+}
+
     int maxProfit(vector<int>& prices) {
-        if(prices.size()==0) return 0;
-        int b1=INT_MAX,p1=INT_MIN,b2=INT_MAX,p2=INT_MIN;
-        for(int i=0;i<prices.size();i++){
-            b1=min(b1,prices[i]);
-            p1=max(p1,prices[i]-b1);
-            b2=min(b2,prices[i]-p1);
-            p2=max(p2,prices[i]-b2);
-        }
-        return p2;
+
+vector<vector<vector<int>>>dp(prices.size() , vector<vector<int>>(2 , vector<int>(3 , -1))) ;
+        int k = answer(prices , 0 , 1 , 2 , dp) ; 
+
+        return k ;
+        
     }
 };
